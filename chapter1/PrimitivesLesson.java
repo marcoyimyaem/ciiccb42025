470946e92f6821b055151d1905b7bba1e95292a3
marcoyimyaem <marcoyimyaem@gmail.com>
2025-06-19 08:01:18 +0000

    day 11

--- chapter1/PrimitivesLesson.java
+++ chapter1/PrimitivesLesson.java
@@ -1,5 +1,6 @@
 package chapter1;
 
+
 public class PrimitivesLesson {
     public static void main(String[] args) {
         System.out.println(Integer.MAX_VALUE);
@@ -8,5 +9,7 @@ public class PrimitivesLesson {
         float f = 3.14F;
         double d = 5.1D;
         long i = 56l;
+        char c = 5657;
+        System.out.println(c);
     }
 }
---
+++ chapter1/ReferenceLesson.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class ReferenceLesson {
+    
+    public static void main(String[] args) {
+        java.util.Date today = new java.util.Date();
+        String greeting = "How are you?"; 
+        // System.out.println(args[1]);
+        Toy _1toy1 = new Toy();
+        Toy toy2 = new Toy();
+        Toy toy3;
+        System.out.println(_1toy1.price);
+
+    }
+}
